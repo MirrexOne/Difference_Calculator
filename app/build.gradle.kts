379110bf409c9dcc88101bec7cefd1f45721835e
Alex Belovitsky <mirrexone@gmail.com>
2024-04-25 10:38:41 +0300

    Add build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,34 @@
+plugins {
+    application
+    checkstyle
+    jacoco
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+    finalizedBy(tasks.jacocoTestReport)
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+}
+
+tasks.getByName("run", JavaExec::class) {
+    standardInput = System.`in`
+}
+
+application {
+    mainClass = "hexlet.code.App"
+}
