4bcc6f684f0363a2d0eca8fbb5c9b6d04d6f03d5
Alex Belovitsky <mirrexone@gmail.com>
2024-04-29 09:35:29 +0300

    Logic by assembling string differences replaced to separate method

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -18,42 +18,43 @@ public class Differ {
     public static String generate(String pathToFile1, String pathToFile2) throws IOException {
         Path normalizePath1 = normalizePath(pathToFile1);
         Path normalizePath2 = normalizePath(pathToFile2);
+
         Map<String, String> parsedJson1 = parse(normalizePath1);
         Map<String, String> parsedJson2 = parse(normalizePath2);
 
-//        System.out.println("Unsorted map 1: " + parsedJson1);
         Map<String, String> sortedMap1 = sortMap(parsedJson1);
-//        System.out.println("Unsorted map 2: " + parsedJson2);
         Map<String, String> sortedMap2 = sortMap(parsedJson2);
 
-//        System.out.println("Sorted map 1: " +  sortedMap1);
-//        System.out.println("Sorted map 2: " +  sortedMap2);
+        return generateJsonDifference(sortedMap1, sortedMap2);
+
+    }
+
+    private static String generateJsonDifference(Map<String, String> jsonData, Map<String, String> jsonData1 ) {
 
         StringBuilder difference = new StringBuilder();
         difference.append("{").append("\n");
 
-        for (Map.Entry<String, String> entry : sortedMap1.entrySet()) {
+        for (Map.Entry<String, String> entry : jsonData.entrySet()) {
             String key1 = entry.getKey();
             String value1 = entry.getValue();
 
-            if ((sortedMap1.containsKey(key1) && sortedMap2.containsKey(key1))
-                    && (sortedMap1.containsValue(value1) && sortedMap2.containsValue(value1))) {
+            if ((jsonData.containsKey(key1) && jsonData1.containsKey(key1))
+                    && (jsonData.containsValue(value1) && jsonData1.containsValue(value1))) {
                 difference.append("    ").append(key1).append(": ").append(value1).append("\n");
-            } else if ((sortedMap1.containsKey(key1) && sortedMap2.containsKey(key1))
-                    && !(sortedMap1.get(key1).equals(sortedMap2.get(key1)))) {
+            } else if ((jsonData.containsKey(key1) && jsonData1.containsKey(key1))
+                    && !(jsonData.get(key1).equals(jsonData1.get(key1)))) {
                 difference.append("  - ").append(key1).append(": ").append(value1).append("\n");
-                difference.append("  + ").append(key1).append(": ").append(sortedMap2.get(key1)).append("\n");
-            } else if (!(sortedMap1.containsKey(key1) && sortedMap2.containsKey(key1))) {
+                difference.append("  + ").append(key1).append(": ").append(jsonData1.get(key1)).append("\n");
+            } else if (!(jsonData.containsKey(key1) && jsonData1.containsKey(key1))) {
                 difference.append("  - ").append(key1).append(": ").append(value1).append("\n");
             }
-
         }
 
-        for (Map.Entry<String, String> entry1 : sortedMap2.entrySet()) {
+        for (Map.Entry<String, String> entry1 : jsonData1.entrySet()) {
             String key2 = entry1.getKey();
             String value2 = entry1.getKey();
 
-            if (!sortedMap1.containsKey(key2)) {
+            if (!jsonData.containsKey(key2)) {
                 difference.append("  + ").append(key2).append(": ").append(value2).append("\n");
             }
 
@@ -61,10 +62,9 @@ public class Differ {
 
         difference.append("}");
 
-//        System.out.println(difference);
         return difference.toString();
-
     }
+
 
     private static Map<String, String> sortMap(Map<String, String> unsortedMap) {
         return unsortedMap.entrySet()
