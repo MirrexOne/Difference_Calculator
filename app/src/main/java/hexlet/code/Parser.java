b69885890c4b8439e592f661505d6ce91bf2f46b
Alex Belovitsky <mirrexone@gmail.com>
2024-04-30 12:11:47 +0300

    Add Parser class

---
+++ app/src/main/java/hexlet/code/Parser.java
@@ -0,0 +1,41 @@
+package hexlet.code;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Parser {
+
+//    private Parser() {
+//    }
+
+    static Map<String, String> parseJson(Path pathToFile) throws IOException {
+        ObjectMapper mapperJson = new ObjectMapper();
+        TypeReference<HashMap<String, String>> specifiedType = new TypeReference<>() {};
+
+        File createdFile = pathToFile.toFile();
+
+        return mapperJson.readValue(createdFile, specifiedType);
+    }
+
+    static Map<String, String> parseYaml(Path pathToFile) throws IOException {
+        ObjectMapper mapperYaml = new YAMLMapper();
+        TypeReference<HashMap<String, String>> specifiedType = new TypeReference<>() {};
+
+        mapperYaml.findAndRegisterModules();
+
+        File createdFile = pathToFile.toFile();
+
+        return mapperYaml.readValue(createdFile, specifiedType);
+    }
+
+//    public static Parser createParser() {
+//        return new Parser();
+//    }
+}
