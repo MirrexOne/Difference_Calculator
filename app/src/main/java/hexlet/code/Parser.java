dd4b8d154ebeaa8578b86fcdbd15bf704256fb0e
Alex Belovitsky <mirrexone@gmail.com>
2024-05-03 18:15:29 +0300

    Refactored Parser class

--- app/src/main/java/hexlet/code/Parser.java
+++ app/src/main/java/hexlet/code/Parser.java
@@ -8,10 +8,17 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
-public class Parser {
+final class Parser {
+
+    private Parser() {
+        throw new IllegalStateException("Utility class");
+    }
 
     static Map<String, Object> parseJson(Path pathToFile) throws IOException {
         File createdFile = pathToFile.toFile();
@@ -25,13 +32,33 @@ public class Parser {
 
     static Map<String, Object> parseYaml(Path pathToFile) throws IOException {
         ObjectMapper mapperYaml = new YAMLMapper();
-        TypeReference<Map<String, Object>> specifiedType = new TypeReference<>() { };
+        TypeReference<Map<String, Object>> specifiedType = new TypeReference<>() {
+        };
 
         mapperYaml.findAndRegisterModules();
 
         File createdFile = pathToFile.toFile();
 
         return mapperYaml.readValue(createdFile, specifiedType);
+    }
+
+    static Path normalizePath(String path) throws IOException {
+        Path normalizeAbsolutePath = Paths.get(path).toAbsolutePath().normalize();
+
+        if (!Files.exists(normalizeAbsolutePath)) {
+            throw new IOException("File '" + normalizeAbsolutePath + "' does not exist");
+        }
+
+        return normalizeAbsolutePath;
+    }
+
+    static Map<String, Object> sortMap(Map<String, Object> unsortedMap) {
+        Map<String, Object> sortedParsedMap = new LinkedHashMap<>();
+        unsortedMap.entrySet().stream()
+                .sorted(Map.Entry.comparingByKey())
+                .forEachOrdered(el -> sortedParsedMap.put(el.getKey(), el.getValue()));
+        return sortedParsedMap;
+
     }
 
 }
