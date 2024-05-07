e86496602c0a976e231986d29790bb9a3e943db9
Alex Belovitsky <mirrexone@gmail.com>
2024-05-07 16:16:59 +0300

    Deleted Parser interface

--- app/src/main/java/hexlet/code/parsers/Parser.java
+++ app/src/main/java/hexlet/code/parsers/Parser.java
@@ -4,6 +4,11 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Map;
 
-public interface Parser {
-    Map<String, Object> parse(File retrievedFileData) throws IOException;
+public abstract class Parser {
+
+    public abstract Map<String, Object> parse(File retrievedFileData) throws IOException;
+
+    public static Parser getParser(String fileExtension) {
+        return "json".equals(fileExtension) ? new Json() : new Yaml();
+    }
 }
