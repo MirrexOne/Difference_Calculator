a95d7c762b676a97d3f5a6e5df99a5bfda819808
Alex Belovitsky <mirrexone@gmail.com>
2024-05-07 16:16:00 +0300

    Deleted ParserFactory class

--- app/src/main/java/hexlet/code/parsers/ParserFactory.java
+++
@@ -1,7 +0,0 @@
-package hexlet.code.parsers;
-
-public interface ParserFactory {
-    static Parser getParser(String fileExtension) {
-        return "json".equals(fileExtension) ? new Json() : new Yaml();
-    }
-}
