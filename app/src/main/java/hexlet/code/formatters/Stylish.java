b211f27d89625db8a98307c45ab3291a8a652349
Alex Belovitsky <mirrexone@gmail.com>
2024-05-08 11:00:12 +0300

    Refactored indents in Stylish class

--- app/src/main/java/hexlet/code/formatters/Stylish.java
+++ app/src/main/java/hexlet/code/formatters/Stylish.java
@@ -18,23 +18,28 @@ public class Stylish extends Format {
             for (Map.Entry<String, Object> pairs : entries) {
                 String key = pairs.getKey();
                 Object value = pairs.getValue();
+
                 if ("unchanged".equals(value)) {
+
                     stylish.append(indents).append("  ").append(key).append(": ")
                             .append(entry.get(keyOfValue)).append("\n");
+
                 } else if ("modified".equals(value)) {
+
                     stylish.append(indents).append("- ").append(key).append(": ")
                             .append(entry.get("value1")).append("\n");
                     stylish.append(indents).append("+ ").append(key).append(": ")
                             .append(entry.get("value2")).append("\n");
 
                 } else if ("added".equals(value)) {
+
                     stylish.append(indents).append("+ ").append(key).append(": ")
                             .append(entry.get(keyOfValue)).append("\n");
 
                 } else if ("deleted".equals(value)) {
+
                     stylish.append(indents).append("- ").append(key).append(": ")
                             .append(entry.get(keyOfValue)).append("\n");
-
                 }
             }
         }
